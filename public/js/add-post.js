e3a4a8ddf003c2f472e82b6f6ef16c4aaa7d2650
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-01 18:10:40 -0400

    make front end js to add a post; referenced activity 10

---
+++ public/js/add-post.js
@@ -0,0 +1,26 @@
+async function newPostEvent(event) {
+  event.preventDefault()
+  const post_subject = document.querySelector('#post_subject').value
+  const post_author = document.querySelector('#post_author').value
+  const post_entry = document.querySelector('#post_entry').value
+
+  const response = await fetch(`/api/entry`, {
+    method: 'POST',
+    body: JSON.stringify({
+      post_subject,
+      post_author,
+      post_entry
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  if (response.ok) {
+    document.location.replace('/')
+  } else {
+    alert('Cannot add post at this time')
+  }
+}
+
+document.querySelector('.***CREATE-FORM-FOR-NEW-POST***')
